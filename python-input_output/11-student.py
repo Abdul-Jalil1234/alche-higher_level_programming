02963013626549b2756fae73352c9a332e20a3ae
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-15 09:45:04 +0400

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -28,6 +28,6 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance using a dictionary."""
+        """Replaces all attributes ."""
         for key, value in json.items():
             setattr(self, key, value)
