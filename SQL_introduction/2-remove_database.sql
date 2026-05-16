86dfda6ae452c04437f2ddb53cde914654f1b439
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-16 10:32:16 +0400

    Update 2-remove_database.sql

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Delete the database hbtn_0c_0 in the MySQL server
 -- This script safely removes the database without failing if it does not exist
-DROP DATABASE IF NOT EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
