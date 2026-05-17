1d4a55bd9360a7367e124e27960708c61611fdcb
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-17 10:53:03 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
 -- Create a MySQL server user with full privileges
--- This script safely provisions user_0d_1 on localhost with all access rights
+-- This script provisions user_0d_1 and explicitly handles password updates if it already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+
