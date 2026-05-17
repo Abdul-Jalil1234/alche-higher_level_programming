9e731e9c13d46336c63e4d06c3b6718c08e49bd3
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-17 10:41:18 +0400

    Create 0-priveleges.sql

---
+++ SQL_more_queries/0-priveleges.sql
@@ -0,0 +1,5 @@
+-- Create a MySQL server user with full privileges
+-- This script safely provisions user_0d_1 on localhost with all access rights
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
