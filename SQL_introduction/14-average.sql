92c68f5f6aed12d5897709fb17296aae14ba3335
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-17 10:29:50 +0400

    Create 14-average.sql

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Compute the score average of all records in second_table
+-- This script calculates the average score and names the result column as average
+SELECT AVG(score) AS average FROM second_table;
+
