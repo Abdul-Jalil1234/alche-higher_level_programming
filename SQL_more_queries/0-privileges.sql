108085f49d1267b88a505bd510a0b6fe0bd5541e
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-17 10:59:17 +0400

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- List all privileges of the MySQL users user_0d_1 and user_0d_2
--- This script displays the grant permissions for both specified users on localhost
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- Query privileges for the first user
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Query privileges for the second user
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
