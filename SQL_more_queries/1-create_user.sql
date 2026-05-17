7f48216c421d9b057d085d17ad9cb9383a203627
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-17 10:51:22 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,3 @@
 -- This script safely provisions user_0d_1 on localhost with all access rights
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
