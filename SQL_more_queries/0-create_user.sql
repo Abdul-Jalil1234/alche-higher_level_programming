50a9b940876d40e6ad2699e2bbfb828e2565079a
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-17 10:42:31 +0400

    Create 0-create_user.sql

---
+++ SQL_more_queries/0-create_user.sql
@@ -0,0 +1,5 @@
+-- List all privileges of the MySQL users user_0d_1 and user_0d_2
+-- This script displays the grant permissions for both specified users on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
