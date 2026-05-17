25d57b430a1a453179418854b023dac03f78da4d
root <a.fuseini@alustudent.com>
2026-05-17 06:40:15 -0700

    Purge dirty directory cache and fix metadata layout for task 0

--- SQL_more_queries/1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- Create a MySQL server user with full privileges
--- This script provisions user_0d_1 on localhost with all access rights
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
